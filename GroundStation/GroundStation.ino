3da67837672529734f4d960577e62eec857a3a99
zhang-yubo <52626825+zhang-yubo@users.noreply.github.com>
2020-01-07 02:12:27 -0500

    new code

---
+++ GroundStation/GroundStation.ino
@@ -0,0 +1,184 @@
+
+#include <RFM69.h>         
+#include <RFM69_ATC.h>     
+#include <SPIFlash.h>
+#include <SPI.h>
+
+#define NODEID        1
+#define NETWORKID     100
+#define FREQUENCY     RF69_433MHZ
+//#define FREQUENCY     RF69_868MHZ
+//#define FREQUENCY     RF69_915MHZ
+#define ENCRYPTKEY    "VCHS" //exactly the same 16 characters/bytes on all nodes!
+#define IS_RFM69HW_HCW  //uncomment only for RFM69HW/HCW! Leave out if you have RFM69W/CW!
+
+
+#define ENABLE_ATC    
+#define SERIAL_BAUD   115200
+
+#if defined (MOTEINO_M0) && defined(SERIAL_PORT_USBVIRTUAL)
+  #define Serial SERIAL_PORT_USBVIRTUAL // Required for Serial on Zero based boards
+#endif
+
+#ifdef ENABLE_ATC
+  RFM69_ATC radio;
+#else
+  RFM69 radio;
+#endif
+
+SPIFlash flash(SS_FLASHMEM, 0xEF30); //EF30 for 4mbit  Windbond chip (W25X40CL)
+bool promiscuousMode = false; //set to 'true' to sniff all packets on the same network
+
+void setup() {
+  Serial.begin(SERIAL_BAUD);
+  delay(10);
+  radio.initialize(FREQUENCY,NODEID,NETWORKID);
+#ifdef IS_RFM69HW_HCW
+  radio.setHighPower(); //must include this only for RFM69HW/HCW!
+#endif
+  radio.encrypt(ENCRYPTKEY);
+  radio.promiscuous(promiscuousMode);
+  //radio.setFrequency(919000000); //set frequency to some custom frequency
+  char buff[50];
+  sprintf(buff, "\nListening at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
+  Serial.println(buff);
+  if (flash.initialize())
+  {
+    Serial.print("SPI Flash Init OK. Unique MAC = [");
+    flash.readUniqueId();
+    for (byte i=0;i<8;i++)
+    {
+      Serial.print(flash.UNIQUEID[i], HEX);
+      if (i!=8) Serial.print(':');
+    }
+    Serial.println(']');
+    
+    //alternative way to read it:
+    //byte* MAC = flash.readUniqueId();
+    //for (byte i=0;i<8;i++)
+    //{
+    //  Serial.print(MAC[i], HEX);
+    //  Serial.print(' ');
+    //}
+  }
+  else
+    Serial.println("SPI Flash MEM not found (is chip soldered?)...");
+    
+#ifdef ENABLE_ATC
+  Serial.println("RFM69_ATC Enabled (Auto Transmission Control)");
+#endif
+}
+
+byte ackCount=0;
+uint32_t packetCount = 0;
+void loop() {
+  //process any serial input
+  if (Serial.available() > 0)
+  {
+    char input = Serial.read();
+    if (input == 'r') //d=dump all register values
+      radio.readAllRegs();
+    if (input == 'E') //E=enable encryption
+      radio.encrypt(ENCRYPTKEY);
+    if (input == 'e') //e=disable encryption
+      radio.encrypt(null);
+    if (input == 'p')
+    {
+      promiscuousMode = !promiscuousMode;
+      radio.promiscuous(promiscuousMode);
+      Serial.print("Promiscuous mode ");Serial.println(promiscuousMode ? "on" : "off");
+    }
+    if (input == 'd') //d=dump flash area
+    {
+      Serial.println("Flash content:");
+      int counter = 0;
+
+      while(counter<=256){
+        Serial.print(flash.readByte(counter++), HEX);
+        Serial.print('.');
+      }
+      while(flash.busy());
+      Serial.println();
+    }
+    if (input == 'D')
+    {
+      Serial.print("Deleting Flash chip ... ");
+      flash.chipErase();
+      while(flash.busy());
+      Serial.println("DONE");
+    }
+    if (input == 'i')
+    {
+      Serial.print("DeviceID: ");
+      word jedecid = flash.readDeviceId();
+      Serial.println(jedecid, HEX);
+    }
+    if (input == 't')
+    {
+      byte temperature =  radio.readTemperature(-1); // -1 = user cal factor, adjust for correct ambient
+      byte fTemp = 1.8 * temperature + 32; // 9/5=1.8
+      Serial.print( "Radio Temp is ");
+      Serial.print(temperature);
+      Serial.print("C, ");
+      Serial.print(fTemp); //converting to F loses some resolution, obvious when C is on edge between 2 values (ie 26C=78F, 27C=80F)
+      Serial.println('F');
+    }
+  }
+
+  if (radio.receiveDone())
+  {
+    Serial.print("#[");
+    Serial.print(++packetCount);
+    Serial.print(']');
+    Serial.print('[');Serial.print(radio.SENDERID, DEC);Serial.print("] ");
+    if (promiscuousMode)
+    {
+      Serial.print("to [");Serial.print(radio.TARGETID, DEC);Serial.print("] ");
+    }
+    parseGPSData();
+    Serial.print("   [RX_RSSI:");Serial.print(radio.RSSI);Serial.print("]");
+    
+    if (radio.ACKRequested())
+    {
+      byte theNodeID = radio.SENDERID;
+      radio.sendACK();
+      Serial.print(" - ACK sent.");
+
+      // When a node requests an ACK, respond to the ACK
+      // and also send a packet requesting an ACK (every 3rd one only)
+      // This way both TX/RX NODE functions are tested on 1 end at the GATEWAY
+      if (ackCount++%3==0)
+      {
+        Serial.print(" Pinging node ");
+        Serial.print(theNodeID);
+        Serial.print(" - ACK...");
+        delay(3); //need this when sending right after reception .. ?
+        if (radio.sendWithRetry(theNodeID, "ACK TEST", 8, 0))  // 0 = only 1 attempt, no retries
+          Serial.print("connected!");
+        else Serial.print("nothing");
+      }
+    }
+    Serial.println();
+    Blink(LED_BUILTIN,3);
+  }
+}
+
+void Blink(byte PIN, int DELAY_MS)
+{
+  pinMode(PIN, OUTPUT);
+  digitalWrite(PIN,HIGH);
+  delay(DELAY_MS);
+  digitalWrite(PIN,LOW);
+}
+
+void parseGPSData(){
+  if (radio.DATA[1]=0)
+    Serial.println("NOT fixed");
+  else{
+    Serial.println("FIXED");
+
+    Serial.print("Longitude:"); 
+    for (byte i = 3; i<16; i++)
+      Serial.print(radio.DATA[i]);
+  }
+}
---
+++ OnBoard/OnBoard.ino
@@ -0,0 +1,250 @@
+
+// Adafruit GPS modules using MTK3329/MTK3339 driver
+
+// Adafruit Ultimate GPS module using MTK33x9 chipset
+//    ------> http://www.adafruit.com/products/746
+
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
+#include <math.h>
+#include <RFM69.h>         
+#include <RFM69_ATC.h>    
+#include <SPIFlash.h>   
+#include <SPI.h>  
+
+
+#define NODEID        2    //unique(range up to 254, 255 is used for broadcast)
+#define NETWORKID     100  //(range up to 255)
+#define GATEWAYID     1
+#define FREQUENCY   RF69_433MHZ
+#define ENCRYPTKEY    "VCHS" //exactly the same 16 characters/bytes on all nodes!
+#define IS_RFM69HW_HCW  //only for RFM69HW/HCW
+#define ENABLE_ATC
+#define ATC_RSSI      -80
+
+
+#define SERIAL_BAUD   115200
+
+#if defined (MOTEINO_M0) && defined(SERIAL_PORT_USBVIRTUAL)
+  #define Serial SERIAL_PORT_USBVIRTUAL // Required for Serial on Zero based boards
+#endif
+
+int TRANSMITPERIOD = 2000; //transmit a packet to gateway so often (in ms)
+char payload[30];
+char buff[20];
+int sendSize;
+boolean requestACK = false;
+boolean newGPSData = false;
+SPIFlash flash(SS_FLASHMEM, 0xEF30); //EF30 for 4mbit  Windbond chip (W25X40CL)
+
+#ifdef ENABLE_ATC
+  RFM69_ATC radio;
+#else
+  RFM69 radio;
+#endif
+
+
+SoftwareSerial GPSerial(3, 2);
+Adafruit_GPS GPS(&GPSerial);
+
+boolean GPSECHO = false;
+
+void setup()
+{
+
+  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
+  // also spit it out
+  Serial.begin(SERIAL_BAUD);
+  Serial.println("Game on");
+
+//radio
+  radio.initialize(FREQUENCY,NODEID,NETWORKID);
+#ifdef IS_RFM69HW_HCW
+  radio.setHighPower(); //must include this only for RFM69HW/HCW!
+#endif
+  radio.encrypt(ENCRYPTKEY);
+#ifdef ENABLE_ATC
+  radio.enableAutoPower(ATC_RSSI);
+#endif
+
+  char buff[50];
+  sprintf(buff, "\nTransmitting at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
+  Serial.println(buff);
+
+  if (flash.initialize())
+  {
+    Serial.print("SPI Flash Init OK ... UniqueID (MAC): ");
+    flash.readUniqueId();
+    for (byte i=0;i<8;i++)
+    {
+      Serial.print(flash.UNIQUEID[i], HEX);
+      Serial.print(' ');
+    }
+    Serial.println();
+  }
+  else
+    Serial.println("SPI Flash MEM not found (is chip soldered?)...");
+
+#ifdef ENABLE_ATC
+  Serial.println("RFM69_ATC Enabled (Auto Transmission Control)\n");
+#endif
+
+
+//GPS
+  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+  GPS.begin(9600);
+
+  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  // uncomment this line to turn on only the "minimum recommended" data
+  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
+  // the parser doesn't care about other sentences at this time
+
+  // Set the update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  // For the parsing code to work nicely and have time to sort thru the data, and
+  // print it out we don't suggest using anything higher than 1 Hz
+
+  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PGCMD_ANTENNA);
+
+  delay(1000);
+  // Ask for firmware version
+  Serial.println(PMTK_Q_RELEASE);
+}
+
+void Blink(byte PIN, int DELAY_MS)
+{
+  pinMode(PIN, OUTPUT);
+  digitalWrite(PIN,HIGH);
+  delay(DELAY_MS);
+  digitalWrite(PIN,LOW);
+}
+
+uint32_t timer = millis();
+void loop()
+{
+  if (Serial.available()>0)
+  {
+    char input = Serial.read();
+    
+    if (input == 'E') 
+    GPSECHO = !GPSECHO;
+
+    if (input >= 48 && input <= 57) //[0,9]
+    {
+      TRANSMITPERIOD = 100 * (input-48);
+      if (TRANSMITPERIOD == 0) TRANSMITPERIOD = 1000;
+      Serial.print("\nChanging delay to ");
+      Serial.print(TRANSMITPERIOD);
+      Serial.println("ms\n");
+    }
+
+  }
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+  if ((c) && (GPSECHO))
+    Serial.write(c);
+
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences!
+    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
+    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
+
+    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+      return;  // we can fail to parse a sentence in which case we should just wait for another
+  }
+
+  if (timer > millis())  timer = millis();
+
+  if (millis() - timer > TRANSMITPERIOD) {
+    timer = millis();
+    
+    if (GPS.fix) 
+    {
+      String data = "";
+//      Serial.print(',');
+//      Serial.print(GPS.fixquality);
+//      Serial.print(',');
+//      
+//      Serial.print(GPS.latitude);
+//      Serial.print(GPS.lat);
+//      Serial.print(',');
+//      
+//      Serial.print(GPS.longitude);
+//      Serial.print(GPS.lon);
+//      Serial.print(',');
+//      
+//      Serial.print(GPS.altitude,3);
+//      Serial.print(',');
+//      
+//      Serial.print(GPS.satellites);
+
+      data += GPS.fix;
+      data += ",";
+      data += GPS.fixquality;
+      data += ",";
+      data += GPS.latitude;
+      data += GPS.lat;
+      data += ",";
+      data += GPS.longitude;
+      data += GPS.lon;
+      data += ",";
+      data += GPS.altitude;
+      data += ",";
+      data += GPS.satellites;
+
+      sendSize = data.length();
+      char payload [sendSize];
+      for (int i=0; i<sendSize; i++)
+      {
+        payload[i]=data.charAt(i);
+      }
+      
+      if (radio.receiveDone())
+      {
+        Serial.print('[');Serial.print(radio.SENDERID, DEC);Serial.print("] ");
+        for (byte i = 0; i < radio.DATALEN; i++)
+          Serial.print((char)radio.DATA[i]);
+        Serial.print("   [RX_RSSI:");Serial.print(radio.RSSI);Serial.print("]");
+      
+        if (radio.ACKRequested())
+        {
+          radio.sendACK();
+          Serial.print(" - ACK sent");
+        }
+        Blink(LED_BUILTIN,3);
+        Serial.println();
+      }
+  
+  
+      //send FLASH id
+      if(sendSize==0)
+      {
+        sprintf(buff, "FLASH_MEM_ID:0x%X", flash.readDeviceId());
+        byte buffLen=strlen(buff);
+        if (radio.sendWithRetry(GATEWAYID, buff, buffLen))
+          Serial.print(" connected!");
+        else Serial.print(" nothing...");
+      }
+      else
+      {
+        Serial.print("Sending[");
+        Serial.print(sendSize);
+        Serial.print("]: ");
+        for(int i = 0; i < sendSize; i++)
+          Serial.print((char)payload[i]);
+  
+        if (radio.sendWithRetry(GATEWAYID, payload, sendSize))
+         Serial.print("sent!");
+        else Serial.print(" nothing...");
+      }
+      Serial.println();
+      Blink(LED_BUILTIN,3);
+    }
+  }
+  
+}
